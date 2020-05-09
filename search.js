54ff5bc348c6998655c253af717c4b8531a906f1
Tarin Pratt <tarinpratt@gmail.com>
2020-05-09 14:59:38 -0700

    add js search functionality

---
+++ search.js
@@ -0,0 +1,87 @@
+'use strict';
+//need to eventually hide these in a private file (technically)
+const key = 'DxMJ184GRqtRPnaqWdp3BbMtG00mbQtE8feSDnTiM6fAHO997P';
+const secret = 'KOE3gOf7D7j36alVVrzPfGLZLzIQjnmXXUcQN4Rx';
+
+function fetchAnimals(fetchUrl) {
+//getting authorization bearer for each separate fetch call
+fetch('https://api.petfinder.com/v2/oauth2/token', {
+	method: 'POST',
+	body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
+	headers: {
+		'Content-Type': 'application/x-www-form-urlencoded'
+	}
+}).then(response => {
+  return response.json()
+}).then(data => {
+  return data
+}).then(data => {
+//fetching url with selected values from the form  
+  return fetch(fetchUrl, {
+    headers: {
+      'Authorization': `${data.token_type} ${data.access_token}`,
+      'Content-Type': 'application/x-www/form-urlencoded'
+    }
+  })
+}).then(response => {
+  return response.json()
+ // running the rendering function 
+}).then(data => displayResults(data.animals))
+.catch(err => {
+  console.log('something went wrong', err)
+})
+}
+
+function displayResults(data) { 
+  $('.results').empty();
+//if the response is empty, render no results found
+data.length === 0 ? $('.results').append(`<p>No results found</p>`) : 
+//otherwise map through and render lists for each animal
+data.map((i) => {
+  i.photos[0] === undefined ? 
+ //list to render if no photos are provided (it was throwing errors with the api) 
+        $('.results').append(`
+    <ul>
+    <li>Name: ${i.name}</li>
+    <li>Breed: ${i.breeds.primary}</li>
+    <li>Gender: ${i.gender}</li>
+    <li>Age: ${i.age}</li>
+    <li><a href=${i.url} target="_blank">More about ${i.name} </a></li>
+    <li><a href='mailto:${i.contact.email}'>Email Shelter</a></li>
+    </ul>
+    `)
+    : 
+  //otherwise render with animal photo
+    $('.results').append(`
+    <ul>
+    <li>Name: ${i.name}</li>
+    <img src='${i.photos[0].medium}' alt='animal'>
+    <li>Breed: ${i.breeds.primary}</li>
+    <li>Gender: ${i.gender}</li>
+    <li>Age: ${i.age}</li>
+    <li><a href=${i.url} target="_blank">More about ${i.name} </a></li>
+    <li><a href='mailto:${i.contact.email}'>Email Shelter</a></li>
+    </ul>
+    `);
+  });
+  $('.results').removeClass('hidden'); 
+}
+//builds the fetch url based off of type of animal and state of location
+function buildUrl(type, location) {
+  let rootUrl = 'https://api.petfinder.com/v2/animals?type=' + type + '&status=adoptable' + '&location=' + location;
+  return rootUrl;
+}
+//getting user values from the form after submission and running the fetch call to api
+function getValues() {
+  $('.petSearch').submit(event => {
+    event.preventDefault();
+    let type = $('#types').val();
+    let location = $('#locations').val();
+    let fetchUrl = buildUrl(type, location);
+    fetchAnimals(fetchUrl);
+  })
+}
+//run everything
+$(function() {
+  getValues();
+});
